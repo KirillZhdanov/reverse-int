70a8e2d4e09bb47e585a881120a5b98f367d3f41
KirillZh <kirill.zhdanov97@gmail.com>
2020-02-20 11:26:07 +0300

    feat: Hope it works like a charm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    n=Math.abs(n);
+    let s=n.toString().split("").reverse();
+    n=Number(s.join(""));
+    console.log("output type is <"+typeof(n)+"> and output value is: "+n)
+    return n;
 }
